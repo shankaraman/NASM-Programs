1b2d2930db889579340ea6f099e55afaddca7028
Shankar Raman Ravindran <s.ravindran@spee.it>
2017-06-14 16:28:17 +0200

    Added resources, tutorial and a program with sys_exit and sys_write

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NASM-Programs
 x86 sample programs
+
+Resources:
+----------
+    + System call prototypes : http://syscalls.kernelgrok.com/
+    + System call table      : https://www.informatik.htw-dresden.de/~beck/ASM/syscall_list.html
+    + Tutorial               : http://asmtutor.com/#lesson1
--- helloworld_exit.asm
+++ helloworld_exit.asm
@@ -1,3 +1,5 @@
+; SYS_WRITE (POSIX) function prototype : ssize_t write(int fd, const void *buf, size_t bytes)
+; SYS_EXIT function prototype : 
 SECTION .data
 msg     db      "Hello World!", 0Ah
 
@@ -6,12 +8,12 @@ global _start
 
 _start:
     
-    mov     edx, 13
-    mov     ecx, msg
-    mov     ebx, 1
-    mov     eax, 4
-    int     80h
+    mov     edx, 13     ; writes 13 bytes. size_t
+    mov     ecx, msg    ; const void *buf. 
+    mov     ebx, 1      ; file descriptor. int fd.
+    mov     eax, 4      ; opcode for sys_write
+    int     80h         ; interrupt signal. passes control to the IVT.
 
-    mov     ebx, 0
+    mov     ebx, 0      ;
     mov     eax, 1
     int     80h
